e8891c6b2a71a49c8a2f6875a3957a10671b67b3
Robert Fisher <slackboy@gmail.com>
2018-08-25 17:05:53 +0100

    move gemrc to ruby manifest

--- modules/basenode/manifests/packages.pp
+++ modules/basenode/manifests/packages.pp
@@ -1,20 +1,12 @@
 #
-# add things we need, remove things we don't. Both lists are defined in
-# hiera.
+# Add things we need, remove things we don't. Both lists are defined
+# in hiera.
 #
 class basenode::packages(
   $installed = undef,
   $absent    = undef,
-) {
-
-  file { '/opt/local/ruby/etc':
-    ensure => directory,
-  }
-
-  file { '/opt/local/ruby/etc/gemrc':
-    source => 'puppet:///modules/basenode/gemrc',
-  }
-
+)
+{
   if $installed {
     package { $installed:
       ensure => installed,
